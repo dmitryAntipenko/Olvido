130fdac0a887a3c8f145ca8e3f1d9508a0eb57fc
mooksel <mooksel@zeouniversity.com>
2016-09-30 20:32:10 +0300

    enemies created

---
+++ Olvido/Olvido/OGEnemy.m
@@ -0,0 +1,63 @@
+//
+//  OGEnemy.m
+//  Olvido
+//
+//  Created by Алексей Подолян on 9/30/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGEnemy.h"
+
+NSString *const kOGEnemyNodeName = @"Enemy Node";
+NSString *const kOGEnemyTextureName = @"EnemyTexture.jpg";
+NSString *const kOGEnemyTextureInvulnerableName = @"EnemyTexture_invulnerable.jpg";
+CGFloat const kOGenemySize = 64;
+CGFloat const kOGInvulnerabilityRepeatCount = 4;
+CGFloat const kOGInvulnerabilityBlinkingTimeDuration = 0.5;
+CGFloat const kOGEnemyVelocity = 20;
+
+@implementation OGEnemy
+
++ (nullable instancetype)enemy
+{
+    OGEnemy *enemy = nil;
+    
+    SKTexture *enemyTexture = [SKTexture textureWithImageNamed:kOGEnemyTextureName];
+    SKTexture *enemyInvulnerableTexture = [SKTexture textureWithImageNamed:kOGEnemyTextureInvulnerableName];
+    
+    if (enemyTexture && enemyInvulnerableTexture)
+    {
+        CGSize size = CGSizeMake(kOGenemySize, kOGenemySize);
+        enemy = [self spriteNodeWithTexture:enemyInvulnerableTexture size:size];
+        
+        if (enemy)
+        {
+            enemy.name = kOGEnemyNodeName;
+            enemy.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:(enemy.size.width / 2.0)];
+//            enemy.physicsBody.linearDamping = 0.0;
+//            enemy.physicsBody.angularDamping = 0.0;
+            enemy.physicsBody.dynamic = YES;
+            enemy.physicsBody.affectedByGravity = NO;
+            
+            SKAction *blink = [SKAction animateWithTextures:@[
+                                                              enemyInvulnerableTexture,
+                                                              enemyTexture
+                                                              ]
+                                               timePerFrame:kOGInvulnerabilityBlinkingTimeDuration];
+            
+            SKAction *repeatBlinking = [SKAction repeatAction:blink count:kOGInvulnerabilityRepeatCount];
+            
+            SKAction *invulnerability = [SKAction sequence:@[
+                                                             repeatBlinking,
+                                                             [SKAction setTexture:enemyTexture]
+                                                             ]];
+            
+            [enemy runAction:invulnerability completion:^{
+                
+            }];
+        }
+    }
+    return enemy;
+}
+
+@end
--- Olvido/Olvido/OGGameScene.m
+++ Olvido/Olvido/OGGameScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "OGGameScene.h"
+#import "OGEnemy.h"
 
 @interface OGGameScene ()
 
@@ -26,6 +27,24 @@
     self.background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     
     [self addChild:self.background];
+    
+    [self createEnemies];
+    
+}
+
+- (void)createEnemies
+{
+    for (int i = 0; i < 5; i++)
+    {
+        OGEnemy *enemy = [OGEnemy enemy];
+        enemy.position = ogRanomPoint(enemy.size.width,
+                                      self.size.width - enemy.size.width,
+                                      enemy.size.width,
+                                      self.size.height - enemy.size.width);
+        [self addChild:enemy];
+        
+        [enemy.physicsBody applyImpulse:ogRanomVector(kOGEnemyVelocity)];
+    }
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
