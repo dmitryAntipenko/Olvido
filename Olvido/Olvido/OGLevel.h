205af51ffda398e8ce86026b72875037a87092dd
mooksel <mooksel@zeouniversity.com>
2016-10-05 18:07:56 +0300

    no message

---
+++ Olvido/Olvido/OGLevel.h
@@ -0,0 +1,13 @@
+//
+//  OGLevel.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 10/5/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OGLevel : NSObject
+
+@end
