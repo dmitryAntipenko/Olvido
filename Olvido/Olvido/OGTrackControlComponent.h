c90913d18c6b4aefa0d3af4d6a2b57ced7700c9f
mooksel <mooksel@zeouniversity.com>
2016-10-17 11:06:04 +0300

    class added

---
+++ Olvido/Olvido/OGTrackControlComponent.h
@@ -0,0 +1,13 @@
+//
+//  OGTrackControlComponent.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 10/17/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGTrackControlComponent : GKComponent
+
+@end
