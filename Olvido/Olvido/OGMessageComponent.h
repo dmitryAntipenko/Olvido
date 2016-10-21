c2529b32c294ad8e30cbe32a4e7b749665bce8a6
Дмитрий Антипенко <vicrattlehead@zeouniversity.com>
2016-10-21 22:01:46 +0300

    Added message component.

---
+++ Olvido/Olvido/OGMessageComponent.h
@@ -0,0 +1,17 @@
+//
+//  OGMessageComponent.h
+//  Olvido
+//
+//  Created by Дмитрий Антипенко on 10/21/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGMessageComponent : GKComponent
+
+- (instancetype)initWithTarget:(SKSpriteNode *)target minShowDistance:(CGFloat)distance;
+
+- (void)addMessage:(NSString *)message forSprite:(SKSpriteNode *)sprite;
+
+@end
