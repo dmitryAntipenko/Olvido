c592f4b5c9f2dbd6fcc2d38568dae2574ca9eaac
Александр Песоцкий <mrdarky@zeocamp-22.zeo.lcl>
2016-10-24 16:02:11 +0300

    use scene creator for chapter1 story

---
+++ Olvido/Olvido/OGChapter1.m
@@ -0,0 +1,13 @@
+//
+//  OGChapter1.m
+//  Olvido
+//
+//  Created by Александр Песоцкий on 10/24/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGChapter1.h"
+
+@implementation OGChapter1
+
+@end
