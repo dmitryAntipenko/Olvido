95a0fc3ddc36bf506da2509d246f8ad68af72898
mooksel <mooksel@zeouniversity.com>
2016-10-26 16:11:20 +0300

    adde base state class

---
+++ Olvido/Olvido/OGLevelState.h
@@ -0,0 +1,13 @@
+//
+//  OGLevelState.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 10/26/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGLevelState : GKState
+
+@end
