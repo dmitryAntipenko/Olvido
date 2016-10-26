e540b3a0187585534c216187f704c368654f8019
Дмитрий Антипенко <vicrattlehead@zeouniversity.com>
2016-10-26 15:37:32 +0300

    Created scene controller class.

---
+++ Olvido/Olvido/OGScenesController.h
@@ -0,0 +1,13 @@
+//
+//  OGScenesController.h
+//  Olvido
+//
+//  Created by Дмитрий Антипенко on 10/26/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OGScenesController : NSObject <OGGameSceneDelegate>
+
+@end
---
+++ Olvido/Olvido/OGScenesController.m
@@ -0,0 +1,13 @@
+//
+//  OGScenesController.m
+//  Olvido
+//
+//  Created by Дмитрий Антипенко on 10/26/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGScenesController.h"
+
+@implementation OGScenesController
+
+@end
