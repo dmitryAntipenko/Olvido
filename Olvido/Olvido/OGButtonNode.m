444cd7716b0acc6aec128c641e109552d125e5ec
mooksel <mooksel@zeouniversity.com>
2016-10-26 10:31:52 +0300

    files added

---
+++ Olvido/Olvido/OGButtonNode.m
@@ -0,0 +1,13 @@
+//
+//  OGButtonNode.m
+//  Olvido
+//
+//  Created by Алексей Подолян on 10/26/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGButtonNode.h"
+
+@implementation OGButtonNode
+
+@end
