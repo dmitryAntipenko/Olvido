863b68eb380d4dda55c21f557e0fbd18f02673ba
mooksel <mooksel@zeouniversity.com>
2016-10-27 16:25:50 +0300

    tap and stop movement control component fixed

--- Olvido/Olvido/OGTapAndStopMovementControlComponent.m
+++ Olvido/Olvido/OGTapAndStopMovementControlComponent.m
@@ -27,7 +27,7 @@ NSString *const kOGTapAndStopMovementControlComponentMovingActionKey = @"movingA
 {
     self.isMooving = YES;
     
-    if (self.visualComponent)
+    if (self.node)
     {
         self.targetPoint = point;
         
@@ -37,17 +37,17 @@ NSString *const kOGTapAndStopMovementControlComponentMovingActionKey = @"movingA
 
 - (void)moveToPoint:(CGPoint)point
 {
-    if (self.visualComponent)
+    if (self.node)
     {
-        [self.visualComponent.spriteNode removeActionForKey:kOGTapAndStopMovementControlComponentMovingActionKey];
+        [self.node removeActionForKey:kOGTapAndStopMovementControlComponentMovingActionKey];
         
-        CGFloat distance = hypot(self.visualComponent.spriteNode.position.x - point.x, self.visualComponent.spriteNode.position.y);
+        CGFloat distance = hypot(self.node.position.x - point.x, self.node.position.y);
         
         CGFloat timeDuration = distance / (self.speedFactor * self.defaultSpeed);
         
         SKAction *movingAction = [SKAction moveTo:point duration:timeDuration];
         
-        [self.visualComponent.spriteNode runAction:movingAction withKey:kOGTapAndStopMovementControlComponentMovingActionKey];
+        [self.node runAction:movingAction withKey:kOGTapAndStopMovementControlComponentMovingActionKey];
     }
 }
 
