4647df5de391276bc0b49bf35bc66b43ef1fab1a
mooksel <mooksel@zeouniversity.com>
2016-11-03 16:10:27 +0200

    start making player entity

---
+++ Olvido/Olvido/OGPlayerEntity.h
@@ -0,0 +1,13 @@
+//
+//  OGPlayerEntity.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 11/3/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGPlayerEntity : GKEntity
+
+@end
