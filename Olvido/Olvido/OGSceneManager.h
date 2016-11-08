65f1e0f1f78564b42014b4a3b6267986b995022f
mooksel <mooksel@zeouniversity.com>
2016-11-08 10:21:51 +0200

    initial commit

---
+++ Olvido/Olvido/OGSceneManager.h
@@ -0,0 +1,14 @@
+//
+//  OGSceneManager.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 11/8/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGSceneManager : NSObject
+
+@end
---
+++ Olvido/Olvido/OGSceneManager.m
@@ -0,0 +1,20 @@
+//
+//  OGSceneManager.m
+//  Olvido
+//
+//  Created by Алексей Подолян on 11/8/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGSceneManager.h"
+
+@interface OGSceneManager ()
+
+@property (nonatomic, retain) SKScene *currentScene;
+
+
+@end
+
+@implementation OGSceneManager
+
+@end
