9636b05ca1a5e31dd4436ed00b66314e620c4cd2
Дмитрий Антипенко <vicrattlehead@zeouniversity.com>
2016-11-22 16:04:41 +0200

    no message

--- Olvido/Olvido/OGGameViewController.m
+++ Olvido/Olvido/OGGameViewController.m
@@ -27,9 +27,7 @@
     
     /* DEBUG OPTIONS */
     view.showsFPS = YES;
-    view.showsNodeCount = YES;
-    
-    view.showsPhysics = YES;
+    view.showsNodeCount = YES;    
     /* DEBUG OPTIONS */
     
     self.sceneManager = [OGSceneManager sceneManagerWithView:view];
