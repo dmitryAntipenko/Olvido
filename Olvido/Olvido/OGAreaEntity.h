8e5da6c14add03b3251ac72f579c6ffdc04a5eb6
mooksel <mooksel@zeouniversity.com>
2016-11-29 21:47:57 +0200

    started

---
+++ Olvido/Olvido/OGAreaEntity.h
@@ -0,0 +1,13 @@
+//
+//  OGAreaEntity.h
+//  Olvido
+//
+//  Created by Алексей Подолян on 11/29/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import <GameplayKit/GameplayKit.h>
+
+@interface OGAreaEntity : GKEntity
+
+@end
---
+++ Olvido/Olvido/OGAreaEntity.m
@@ -0,0 +1,13 @@
+//
+//  OGAreaEntity.m
+//  Olvido
+//
+//  Created by Алексей Подолян on 11/29/16.
+//  Copyright © 2016 Дмитрий Антипенко. All rights reserved.
+//
+
+#import "OGAreaEntity.h"
+
+@implementation OGAreaEntity
+
+@end
